0351aa430659d1ea5318d5a8cf43c421ce67f162
QuirozE <equiroz@ciencias.unam.mx>
2019-10-27 19:46:58 -0600

    Añadiendo caso para Fix y mostrando errores más amigables

--- BAE/src/BAE/Dynamic.hs
+++ BAE/src/BAE/Dynamic.hs
@@ -13,23 +13,10 @@ Sandra del Mar Soto Corderi
 
 module BAE.Dynamic where
 
-  import Data.List
+  import BAE.Sintax
+  import qualified BAE.Static as Static
+  import qualified BAE.Type as Type
 
-  import  BAE.Sintax as Sintax
-  import  BAE.Static as Static
-  import  BAE.Type as Type
-
-
-  -- | Determina si una expresión está en forma normal, es decir,
-  --no se pueden hacer más beta reducciones.
-  normal :: Expr -> Bool
-  normal (Fn _ e) = normal e
-  normal (Fix _ _) = False
-  normal (App (Fn _ _) e2) = False
-  normal (App e1 e2) = (normal e1) && (normal e2)
-  normal _ = False
-
-  -- | Devuelve la transición tal que eval1 e = e' syss e -> e' .
   eval1 :: Expr -> Expr
   eval1 expr =
     case expr of
@@ -66,74 +53,74 @@ module BAE.Dynamic where
       Eq e1 e2 -> let e1' = eval1 e1 in Eq e1' e2
       If (B q) e1 e2 -> if q then e1 else e2
       If e1 e2 e3 -> If (eval1 e1) e2 e3
-      Let i e1 e2 ->
-        if locked e1
+      Let i e1 e2 -> 
+        if blocked e1
           then subst e2 (i, e1)
           else Let i (eval1 e1) e2
       Fn x e1 ->  Fn x (eval1 e1)
-      App f@(Fn x e3) e2 ->
-        if locked e2
+      Fix f e1 -> Fn f (Fix f e1)
+      App f@(Fn x e3) e2 -> 
+        if blocked e2
           then subst e3 (x, e2)
           else App f (eval1 e2)
       App e1 e2 -> App (eval1 e1) e2
+        
 
-  -- | Indica si la transicion a evaluar esta bloqueada.
-  locked :: Expr -> Bool
-  locked expr =
+  blocked :: Expr -> Bool
+  blocked expr =
     case expr of
       I n -> True
       B p -> True
       V x -> True
       Add (I _) (I _) -> False
-      Add (I _) e -> locked e
-      Add e1 e2 -> locked e1
+      Add (I _) e -> blocked e
+      Add e1 e2 -> blocked e1
       Mul (I _) (I _) -> False
-      Mul (I _) e -> locked e
-      Mul e1 e2 -> locked e1
+      Mul (I _) e -> blocked e
+      Mul e1 e2 -> blocked e1
       Succ (I _) -> False
-      Succ e -> locked e
+      Succ e -> blocked e
       Pred (I 0) -> False
       Pred (I n) -> False
-      Pred e -> locked e
+      Pred e -> blocked e
       Not (B p) -> False
-      Not e -> locked e
+      Not e -> blocked e
       And (B p) (B q) -> False
-      And (B p) e -> locked e
-      And e1 e2 -> locked e1
+      And (B p) e -> blocked e
+      And e1 e2 -> blocked e1
       Or (B p) (B q) -> False
-      Or (B p) e -> locked e
-      Or e1 e2 -> locked e1
+      Or (B p) e -> blocked e
+      Or e1 e2 -> blocked e1
       Lt (I n) (I m) -> False
-      Lt (I n) e -> locked e
-      Lt e1 e2 -> locked e1
+      Lt (I n) e -> blocked e
+      Lt e1 e2 -> blocked e1
       Gt (I n) (I m) -> False
-      Gt (I n) e -> locked e
-      Gt e1 e2 -> locked e1
+      Gt (I n) e -> blocked e
+      Gt e1 e2 -> blocked e1
       Eq (I n) (I m) -> False
-      Eq (I n) e -> locked e
-      Eq e1 e2 -> locked e1
+      Eq (I n) e -> blocked e
+      Eq e1 e2 -> blocked e1
       If (B q) e1 e2 -> False
-      If e1 e2 e3 -> locked e1
+      If e1 e2 e3 -> blocked e1
       Let i e1 e2 -> False
-      Fn i e1 -> locked e1
-      App e1 e2 ->
-        if locked e1
-          then
-            if locked e2
+      Fn i e1 -> blocked e1
+      Fix _ _ ->False
+      App e1 e2 -> 
+        if blocked e1
+          then 
+            if blocked e2
               then case e1 of
                 Fn _ _ -> False
                 _ -> True
               else False
           else False
 
-  -- | Devuelve la transición tal que eval1 e = e' syss e ->* e' y e' esta bloqueado.
   evals :: Expr -> Expr
   evals expr =
-    if locked expr
+    if blocked expr
       then expr
       else evals (eval1 expr)
 
-  -- | Devuelve la transición tal que eval1 e = e' syss e ->* e' y e' es un valor.
   evale :: Expr -> Expr
   evale ex =
     case evals ex of
@@ -155,50 +142,12 @@ module BAE.Dynamic where
       Fn _ _ -> error "[Fn] Expected argument"
       App _ _ -> error "[App] Expected function as first argument"
 
-{--
-  -- | Verifica el tipado de un programa tal que vt F e T = True syss F |- e : T.
-  vt :: Ctx -> Expr -> Type -> Bool
-  vt ctx (V i) t = searchDecl ctx i t
-  vt ctx (If e1 e2 e3) t = (vt ctx e1 Boolean) && (vt ctx e1 t) && (vt ctx e1 t)
-  vt ctx (Let i e1 e2) t =
-      if (vt ctx e1 Integer)
-        then vt ((i, Integer):ctx) e2 t
-        else vt ((i, Boolean):ctx) e2 t
-  vt ctx e t =
-    case t of
-      Integer -> case e of
-        I n -> True
-        Add e1 e2 -> (vt ctx e1 t) && (vt ctx e2 t)
-        Mul e1 e2 -> (vt ctx e1 t) && (vt ctx e2 t)
-        Succ e -> vt ctx e t
-        Pred e -> vt ctx e t
-        _ -> False
-      Boolean -> case e of
-        B p -> True
-        Not e -> vt ctx e t
-        And e1 e2 -> (vt ctx e1 t) && (vt ctx e2 t)
-        Or e1 e2 -> (vt ctx e1 t) && (vt ctx e2 t)
-        Lt e1 e2 -> (vt ctx e1 Integer) && (vt ctx e2 Integer)
-        Gt e1 e2 -> (vt ctx e1 Integer) && (vt ctx e2 Integer)
-        Eq e1 e2 -> (vt ctx e1 Integer) && (vt ctx e2 Integer)
-        _ -> False
-
-  -- | Busca la declaración de una variable en un contexto.
-  searchDecl :: Ctx -> Identifier -> Type -> Bool
-  searchDecl [] _ _  = False
-  searchDecl ((i', t'):xs) i t =
-    if i == i'
-      then t == t'
-      else searchDecl xs i t
---}
-
-  -- | Verifica el tipado de un programa, y en caso de ser correcto lo evalúa hasta obtener un valor.
-  eval :: Expr -> Type -> Expr
-  eval e t = let (g, t') = infer e
-             in
-                if (g /= [] ) then
-                  error "Expression with unbound variables"
-                else if (t' /= t) then
-                  error "Type error"
-                else
-                  evale e
+  eval :: Expr -> Type.Type -> Expr
+  eval e t =
+    let (ctx, t') = Static.infer e
+      in
+        if ctx /= [] then error ("Expression with unbounded variables: " ++ (show ctx))
+        else 
+          if (t /= t')
+            then error ("Type error: " ++ (show t) ++ " is not " ++ (show t'))
+            else evale e 
